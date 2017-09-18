7774f672bb6e36afd3c9d257aded7eabe6421c6f
tarzansos <tuminfei1981@gmail.com>
2017-09-18 18:01:06 +0800

    first commit

--- lib/acts_as_id_bank_certification/holder.rb
+++ lib/acts_as_id_bank_certification/holder.rb
@@ -4,12 +4,16 @@ module ActsAsIdBankCertification
     def self.included(base)
       # allow user to define these
       aliases = {
-          id_bank_certification_on?: [:id_bank_certification_for?]
+          id_bank_certification_verified?: [:id_bank_certification_pass?]
       }
 
       base.class_eval do
 
         has_one :id_bank_document, class_name: "ActsAsIdBankCertification::IdBankDocument", as: :holder, dependent: :destroy
+
+        before_create :build_default_id_bank_document
+
+        delegate :verified?,  to: :id_bank_document, prefix: true, allow_nil: true
 
         aliases.each do |method, links|
           links.each do |new_method|
@@ -19,8 +23,14 @@ module ActsAsIdBankCertification
       end
     end
 
-    def id_bank_certification_on?
-      id_bank_document.holder
+    def id_bank_certification_verified?
+      id_bank_document.verified?
+    end
+
+    def build_default_id_bank_document
+      #默认是身份证
+      build_id_bank_document(:card_type => ActsAsIdBankCertification::IdBankDocument::CARD_TYPE_DEBIT)
+      true
     end
   end
 end
